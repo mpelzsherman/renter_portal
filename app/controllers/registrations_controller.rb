6bc0508430774922db267ecf3a03489aa30ced31
mpelzsherman <mpelzsherman@gmail.com>
2014-05-07 19:21:22 -0400

    Fixed nested attribute handling in devise registration process.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  private
+
   def sign_up_params
-    allow = [:email, :password, :password_confirmation, account: [:first_name, :last_name, :street, :city, :state, :zip]]
+    allow = [:email, :password, :password_confirmation, account_attributes: [:first_name, :last_name, :street, :city, :state, :zip]]
     params.require(resource_name).permit(allow)
   end
 
