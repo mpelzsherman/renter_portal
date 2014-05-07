6239bd475ec82b83140c4f87a5320c364bfaaaa4
Bethurel Griffin Jr <bethurel.griffin.jr@gmail.com>
2014-05-07 18:59:35 -0400

    [BG] [000000] Added State Dropdown to Form.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module RenterPortal
   class Application < Rails::Application
+    state_hash = YAML.load(File.open("#{Rails.root}/config/states.yml"))
+    config.states = state_hash['states']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
