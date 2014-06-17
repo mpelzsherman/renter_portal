dfa6af201ada95f18302d4729dc5a10b29382ada
Luke Tower <lukegtower@gmail.com>
2014-06-17 16:21:56 -0400

    [LT][000000] Fix update and delete service request emails to use name

--- app/controllers/service_requests_controller.rb
+++ app/controllers/service_requests_controller.rb
@@ -61,7 +61,7 @@ class ServiceRequestsController < ApplicationController
       if @service_request.update(service_request_params)
         format.html { redirect_to @service_request, notice: 'Service request was successfully updated.' }
         format.json { render :show, status: :ok, location: @service_request }
-        ServiceRequestsMailer.send_update_service_request_email(@service_request).deliver
+        ServiceRequestsMailer.send_update_service_request_email(@service_request, current_user.account.full_name).deliver
       else
         format.html { render :edit }
         format.json { render json: @service_request.errors, status: :unprocessable_entity }
@@ -72,7 +72,7 @@ class ServiceRequestsController < ApplicationController
   # DELETE /service_requests/1
   # DELETE /service_requests/1.json
   def destroy
-    ServiceRequestsMailer.send_delete_service_request_email(@service_request).deliver
+    ServiceRequestsMailer.send_delete_service_request_email(@service_request, current_user.account.full_name).deliver
     @service_request.destroy
     respond_to do |format|
       format.html { redirect_to service_requests_url, notice: 'Service request was successfully destroyed.' }
